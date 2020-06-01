fc10c38090857034facd6159b6469c9fb6763129
Arnaud Gissinger <agissing@student.42.fr>
2020-06-01 10:18:15 +0200

    [FIX] setup.py issue

--- setup.py
+++ setup.py
@@ -1,26 +1,15 @@
 import setuptools
 
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-reqs = parse_requirements("requirements.txt", session=False)
-install_requires = [str(ir.req) for ir in reqs]
-
 setuptools.setup(
     name="notion",
     version="0.0.25",
     author="Jamie Alexandre",
     author_email="jamalex+python@gmail.com",
     description="Unofficial Python API client for Notion.so",
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/jamalex/notion-py",
-    install_requires=install_requires,
+    url="https://github.com/mathix420/notion-py",
+    install_requires=open('requirements.txt').read().split('\n'),
     include_package_data=True,
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
